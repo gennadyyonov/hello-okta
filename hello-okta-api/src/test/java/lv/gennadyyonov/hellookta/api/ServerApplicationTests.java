c893321eda3f28fa07d11f0c51a66790a1a414a3
S03X6Q <Gennady_Yonov@rcomext.com>
2020-01-31 21:46:05 +0200

    Run application locally: -- application properties -- feign configuration -- java 8 compatibility

--- hello-okta-api/src/test/java/lv/gennadyyonov/hellookta/api/ServerApplicationTests.java
+++ hello-okta-api/src/test/java/lv/gennadyyonov/hellookta/api/ServerApplicationTests.java
@@ -2,11 +2,20 @@ package lv.gennadyyonov.hellookta.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(
+        properties = {
+                "issuer=https://dev-220281.okta.com/oauth2/aus26efk9hrb1yASy357",
+                "clientId=0oa26efk122CnG3k3357",
+                "clientSecret=f-Th4oNxu0qzMghhtGev2vaoAPf9l7IgzHENsYny",
+                "audience=api://hellookta"
+        }
+)
 @SpringBootTest
 class ServerApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 }
--- hello-okta-bff/src/test/java/lv/gennadyyonov/hellookta/bff/ClientApplicationTest.java
+++ hello-okta-bff/src/test/java/lv/gennadyyonov/hellookta/bff/ClientApplicationTest.java
@@ -2,7 +2,19 @@ package lv.gennadyyonov.hellookta.bff;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
+@TestPropertySource(
+        properties = {
+                "issuer=https://dev-220281.okta.com/oauth2/aus26efk9hrb1yASy357",
+                "clientId=0oa26efk122CnG3k3357",
+                "clientSecret=f-Th4oNxu0qzMghhtGev2vaoAPf9l7IgzHENsYny",
+                "audience=api://hellookta",
+                "hellooktaclient_id=0oa2m950mjcFnPPNJ357",
+                "hellooktaclient_secret=IHiSqDkab2oh_pQXAscZ4BuRUb1X0yEHYl70cVgI",
+                "hellooktaapiclient_baseUrl=http://localhost:8070"
+        }
+)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ClientApplicationTest {
 
