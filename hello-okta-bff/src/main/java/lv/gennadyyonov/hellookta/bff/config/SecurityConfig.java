95461fa88a37ee3b4bd2f1efc75566fb3687f5f1
gennadyyonov <g.jonovs@mail.ru>
2020-03-01 15:27:30 +0200

    env config properties exposed as rest end-point

--- hello-okta-bff/src/main/java/lv/gennadyyonov/hellookta/bff/config/SecurityConfig.java
+++ hello-okta-bff/src/main/java/lv/gennadyyonov/hellookta/bff/config/SecurityConfig.java
@@ -4,22 +4,26 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsUtils;
 
-import static org.springframework.http.HttpMethod.OPTIONS;
+import static lv.gennadyyonov.hellookta.bff.controller.EnvironmentConfigController.ENVIRONMENT_CONFIG_SUFFIX;
+import static org.springframework.http.HttpMethod.GET;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private static final String ALL_URL_PATTERN = "/**";
-
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
+                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .and()
                 .csrf().disable()
                 .authorizeRequests()
                 // Allow CORS option calls
-                .antMatchers(OPTIONS, ALL_URL_PATTERN).permitAll()
+                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers(GET, ENVIRONMENT_CONFIG_SUFFIX).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
--- hello-okta-services/src/main/java/lv/gennadyyonov/hellookta/utils/SecurityUtils.java
+++ hello-okta-services/src/main/java/lv/gennadyyonov/hellookta/utils/SecurityUtils.java
@@ -6,6 +6,7 @@ import lv.gennadyyonov.hellookta.dto.UserInfo;
 import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
+import static lv.gennadyyonov.hellookta.constants.SecurityConstants.PUBLIC_ENDPOINT;
 
 @UtilityClass
 public class SecurityUtils {
@@ -16,5 +17,9 @@ public class SecurityUtils {
 
     public boolean hasAnyRoles(UserInfo profile, Stream<String> rolesStream) {
         return rolesStream.anyMatch(role -> profile.getRoles().contains(role));
+    }
+
+    public boolean shouldBypassSecurityValidation(String alias) {
+        return PUBLIC_ENDPOINT.equals(alias);
     }
 }
