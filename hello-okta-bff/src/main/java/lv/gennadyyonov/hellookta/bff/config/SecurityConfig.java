32094297902e85a50f5c735cc405272105bf5bad
gennadyyonov <g.jonovs@mail.ru>
2020-03-01 16:35:53 +0200

    env config properties exposed as rest end-point: CORS

--- hello-okta-bff/src/main/java/lv/gennadyyonov/hellookta/bff/config/SecurityConfig.java
+++ hello-okta-bff/src/main/java/lv/gennadyyonov/hellookta/bff/config/SecurityConfig.java
@@ -1,32 +1,69 @@
 package lv.gennadyyonov.hellookta.bff.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsUtils;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.singletonList;
+import static java.util.stream.Collectors.toList;
 import static lv.gennadyyonov.hellookta.bff.controller.EnvironmentConfigController.ENVIRONMENT_CONFIG_SUFFIX;
+import static org.springframework.http.HttpHeaders.AUTHORIZATION;
+import static org.springframework.http.HttpHeaders.CACHE_CONTROL;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.HttpHeaders.PRAGMA;
 import static org.springframework.http.HttpMethod.GET;
+import static org.springframework.http.HttpMethod.OPTIONS;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private static final String ALL = "*";
+    private static final String ALL_URL_PATTERN = "/**";
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
-                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+                .cors()
                 .and()
                 .csrf().disable()
                 .authorizeRequests()
                 // Allow CORS option calls
-                .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
+                .antMatchers(OPTIONS, ALL_URL_PATTERN).permitAll()
                 .antMatchers(GET, ENVIRONMENT_CONFIG_SUFFIX).permitAll()
                 .anyRequest()
                 .authenticated()
                 .and()
                 .oauth2Login();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        final CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(singletonList(ALL));
+        List<String> allowedMethods = Arrays.stream(HttpMethod.values())
+                .map(HttpMethod::name)
+                .collect(toList());
+        configuration.setAllowedMethods(allowedMethods);
+        // setAllowCredentials(true) is important, otherwise:
+        // The value of the 'Access-Control-Allow-Origin' header in the response must not be the wildcard '*'
+        // when the request's credentials mode is 'include'.
+        configuration.setAllowCredentials(true);
+        // setAllowedHeaders is important! Without it, OPTIONS preflight request
+        // will fail with 403 Invalid CORS request
+        configuration.setAllowedHeaders(asList(AUTHORIZATION, CONTENT_TYPE, CACHE_CONTROL, PRAGMA));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration(ALL_URL_PATTERN, configuration);
+        return source;
     }
 }
