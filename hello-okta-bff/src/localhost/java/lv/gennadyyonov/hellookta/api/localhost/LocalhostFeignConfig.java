69069cb08686ce0c9c111e646c0ea8bcdfce734e
gennadyyonov <g.jonovs@mail.ru>
2020-11-15 17:47:53 +0200

    -- fix gradle 7.0 warnings

--- hello-okta-bff/src/localhost/java/lv/gennadyyonov/hellookta/api/localhost/LocalhostFeignConfig.java
+++ hello-okta-bff/src/localhost/java/lv/gennadyyonov/hellookta/api/localhost/LocalhostFeignConfig.java
@@ -1,21 +1,26 @@
 package lv.gennadyyonov.hellookta.api.localhost;
 
 import brave.http.HttpTracing;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import lv.gennadyyonov.hellookta.config.feign.FeignClientProvider;
 import lv.gennadyyonov.hellookta.config.feign.LocalhostFeignClientProvider;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 
+import javax.annotation.PostConstruct;
+
+@Slf4j
+@RequiredArgsConstructor
 @Configuration
 public class LocalhostFeignConfig {
 
     private final HttpTracing httpTracing;
 
-    @Autowired
-    public LocalhostFeignConfig(HttpTracing httpTracing) {
-        this.httpTracing = httpTracing;
+    @PostConstruct
+    void init() {
+        log.info("Initializing Localhost Feign Configuration...");
     }
 
     @Primary
