fe61815ae4c9d084fe6e2ab69eb171a0a0d53580
gennadyyonov <g.jonovs@mail.ru>
2020-11-16 21:36:57 +0200

    -- refresh jwk set cache async

--- hello-okta-api/src/main/java/lv/gennadyyonov/hellookta/api/ServerApplication.java
+++ hello-okta-api/src/main/java/lv/gennadyyonov/hellookta/api/ServerApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @EnableConfigurationProperties({HelloOktaApiProps.class})
 @SpringBootApplication
 public class ServerApplication extends SpringBootServletInitializer {
