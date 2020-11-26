da1524f4f11372742e0c847fc17b9741c723d85e
Gennady Yonov <g.jonovs@mail.ru>
2020-11-26 12:16:41 +0200

    Hello okta docker (#19)
    * docker containerize (api, bff)
    
    * docker containerize (api, bff)
    
    * performance improvement
    
    * docker-compose

---
+++ hello-okta-services/src/main/java/lv/gennadyyonov/hellookta/config/EnvironmentProfiles.java
@@ -0,0 +1,11 @@
+package lv.gennadyyonov.hellookta.config;
+
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
+public class EnvironmentProfiles {
+
+    public static final String LOCALHOST = "localhost";
+    public static final String NOT = "!";
+    public static final String NOT_LOCALHOST = NOT + LOCALHOST;
+}
