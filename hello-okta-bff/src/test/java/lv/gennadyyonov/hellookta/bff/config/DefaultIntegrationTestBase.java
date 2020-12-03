abfa2d588be73d3555f450ffa50bc08d651aaeac
gennadyyonov <g.jonovs@mail.ru>
2020-12-03 08:33:01 +0200

    -- possibility to have unauth test calls

--- hello-okta-bff/src/test/java/lv/gennadyyonov/hellookta/bff/config/DefaultIntegrationTestBase.java
+++ hello-okta-bff/src/test/java/lv/gennadyyonov/hellookta/bff/config/DefaultIntegrationTestBase.java
@@ -2,8 +2,10 @@ package lv.gennadyyonov.hellookta.bff.config;
 
 import com.github.tomakehurst.wiremock.WireMockServer;
 import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
@@ -12,15 +14,25 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public abstract class DefaultIntegrationTestBase {
 
     @Autowired
+    private TestRestTemplateConfigurer restTemplateConfigurer;
+    @Autowired
+    private TestRestTemplate restTemplate;
+    @Autowired
     private WireMockServer wireMockServer;
 
     @BeforeEach
     void setUp() {
+        restTemplateConfigurer.setUp(restTemplate);
         wireMockServer.stubFor(
                 WireMock.get("/okta/oauth2/default/v1/userinfo")
                         .willReturn(aResponse()
                                 .withHeader("Content-Type", APPLICATION_JSON_VALUE)
                                 .withBodyFile("okta/oauth2/userinfo.json"))
         );
+    }
+
+    @AfterEach
+    void tearDown() {
+        restTemplateConfigurer.reset(restTemplate);
     }
 }
